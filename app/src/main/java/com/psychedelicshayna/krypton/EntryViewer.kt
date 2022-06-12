0399ebedf1b3acf83ee4eeb73c7aafe2e111a5c4
PsychedelicShayna <0x0000@tuta.io>
2022-06-11 22:21:47 -0300

    Added password generator support

--- app/src/main/java/com/psychedelicshayna/krypton/EntryViewer.kt
+++ app/src/main/java/com/psychedelicshayna/krypton/EntryViewer.kt
@@ -8,10 +8,7 @@ import android.view.ContextMenu
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_entry_viewer.*
@@ -81,6 +78,108 @@ class EntryViewer : AppCompatActivity() {
         }
     }
 
+    private fun showPasswordGeneratorDialog(insertToNameCallback: (AlertDialog, String) -> Unit, insertToValueCallback: (AlertDialog, String) -> Unit) {
+        val dialogEntriesPasswordGenerator: View = LayoutInflater.from(this).inflate(
+            R.layout.dialog_entries_password_generator,
+            null
+        )
+
+        val buttonGeneratePassword: Button =
+            dialogEntriesPasswordGenerator.findViewById(R.id.dialogEntriesPasswordGeneratorButtonGenerate) ?: run {
+                Toast.makeText(this, "Error! Could not find the view for the generate button.", Toast.LENGTH_LONG).show()
+                return
+            }
+
+        val buttonInsertToName: Button =
+            dialogEntriesPasswordGenerator.findViewById(R.id.dialogEntriesPasswordGeneratorButtonInsertName) ?: run {
+                Toast.makeText(this, "Error! Could not find the view for the insert to name button.", Toast.LENGTH_LONG).show()
+                return
+            }
+
+        val buttonInsertToValue: Button =
+            dialogEntriesPasswordGenerator.findViewById(R.id.dialogEntriesPasswordGeneratorButtonInsertValue) ?: run {
+                Toast.makeText(this, "Error! Could not find the view for the insert to value button.", Toast.LENGTH_LONG).show()
+                return
+            }
+
+        val editTextPasswordLength: EditText =
+            dialogEntriesPasswordGenerator.findViewById(R.id.dialogEntriesPasswordGeneratorEditTextLength) ?: run {
+                Toast.makeText(this, "Error! Could not find the view for the password length edit text.", Toast.LENGTH_LONG).show()
+                return
+            }
+
+        val editTextGeneratedPassword: EditText =
+            dialogEntriesPasswordGenerator.findViewById(R.id.dialogEntriesPasswordGeneratorEditTextPassword) ?: run {
+                Toast.makeText(this, "Error! Could not find the view for the password length edit text.", Toast.LENGTH_LONG).show()
+                return
+            }
+
+        val checkBoxUpper: CheckBox =
+            dialogEntriesPasswordGenerator.findViewById(R.id.dialogEntriesPasswordGeneratorCheckBoxUpper) ?: run {
+                Toast.makeText(this, "Error! Could not find the view for the uppercase checkbox.", Toast.LENGTH_LONG).show()
+                return
+            }
+
+        val checkBoxLower: CheckBox =
+            dialogEntriesPasswordGenerator.findViewById(R.id.dialogEntriesPasswordGeneratorCheckBoxLower) ?: run {
+                Toast.makeText(this, "Error! Could not find the view for the lowercase checkbox.", Toast.LENGTH_LONG).show()
+                return
+            }
+
+        val checkBoxNumerical: CheckBox =
+            dialogEntriesPasswordGenerator.findViewById(R.id.dialogEntriesPasswordGeneratorCheckBoxNumeric) ?: run {
+                Toast.makeText(this, "Error! Could not find the view for the numerical checkbox.", Toast.LENGTH_LONG).show()
+                return
+            }
+
+        val checkBoxSpecial: CheckBox =
+            dialogEntriesPasswordGenerator.findViewById(R.id.dialogEntriesPasswordGeneratorCheckBoxSpecial) ?: run {
+                Toast.makeText(this, "Error! Could not find the view for the special characters checkbox.", Toast.LENGTH_LONG).show()
+                return
+            }
+
+        val checkBoxExtra: CheckBox =
+            dialogEntriesPasswordGenerator.findViewById(R.id.dialogEntriesPasswordGeneratorCheckBoxExtra) ?: run {
+                Toast.makeText(this, "Error! Could not find the view for the extra characters checkbox.", Toast.LENGTH_LONG).show()
+                return
+            }
+
+        val alertDialogBuilder: AlertDialog.Builder = AlertDialog.Builder(this).apply {
+            setView(dialogEntriesPasswordGenerator)
+            setCancelable(true)
+            setTitle("Password Generator")
+            setNegativeButton("Cancel") { _, _ -> }
+        }
+
+        val alertDialog: AlertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+
+        buttonGeneratePassword.setOnClickListener {
+            val passwordGenerator = PasswordGenerator()
+
+            val characterClassSpec: PasswordGenerator.CharacterClassSpec = PasswordGenerator.CharacterClassSpec().apply {
+                uppercase.enabled = checkBoxUpper.isChecked
+                lowercase.enabled = checkBoxLower.isChecked
+                numerical.enabled = checkBoxNumerical.isChecked
+                special.enabled   = checkBoxSpecial.isChecked
+                extra.enabled     = checkBoxExtra.isChecked
+            }
+
+            val passwordLength: Int = editTextPasswordLength.text.toString().toInt()
+            val generatedPassword: String = passwordGenerator.generatePassword(passwordLength, characterClassSpec)
+
+            editTextGeneratedPassword.setText(generatedPassword)
+        }
+
+        buttonInsertToName.setOnClickListener {
+            insertToNameCallback.invoke(alertDialog, editTextGeneratedPassword.text.toString())
+        }
+
+        buttonInsertToValue.setOnClickListener {
+            insertToValueCallback.invoke(alertDialog, editTextGeneratedPassword.text.toString())
+        }
+    }
+
     private fun editEntry(position: Int) {
         val entry: Pair<String, String> = entryAdapter[position] ?: run {
             Toast.makeText(this, "Error! Cannot access the entry at index $position, index out of range!", Toast.LENGTH_LONG).show()
@@ -92,30 +191,83 @@ class EntryViewer : AppCompatActivity() {
             null
         )
 
-        val dialogEntryValuesInputEditTextEntryName: EditText =
+        val editTextEntryName: EditText =
             dialogEntryValuesInput.findViewById(R.id.dialogEntryValuesInputEditTextEntryName) ?: run {
                 Toast.makeText(this, "Error! Could not find the view for the entry name EditText!", Toast.LENGTH_SHORT).show()
                 return@editEntry
             }
 
-        val dialogEntryValuesInputEditTextEntryValue: EditText =
+        val editTextEntryValue: EditText =
             dialogEntryValuesInput.findViewById(R.id.dialogEntryValuesInputEditTextEntryValue) ?: run {
                 Toast.makeText(this, "Error! Could not find the view for the entry value EditText!", Toast.LENGTH_SHORT).show()
                 return@editEntry
             }
 
-        val dialogEntryValuesInputButtonPasswordGenerator: Button =
+        val buttonPasswordGenerator: Button =
             dialogEntryValuesInput.findViewById(R.id.dialogEntryValuesInputButtonPasswordGenerator) ?: run {
                 Toast.makeText(this, "Error! Could not find the view for the password generator button!", Toast.LENGTH_SHORT).show()
                 return@editEntry
             }
 
-        dialogEntryValuesInputButtonPasswordGenerator.setOnClickListener {
+        editTextEntryName.setText(entry.first)
+        editTextEntryValue.setText(entry.second)
 
+        val alertDialogBuilder: AlertDialog.Builder = AlertDialog.Builder(this).apply {
+            setView(dialogEntryValuesInput)
+            setCancelable(true)
+
+            setTitle("Enter New Entry Values")
+
+            setPositiveButton("Okay")     { _, _  -> }
+            setNegativeButton("Cancel")  { _ , _ -> }
         }
 
-        dialogEntryValuesInputEditTextEntryName.setText(entry.first)
-        dialogEntryValuesInputEditTextEntryValue.setText(entry.second)
+        val alertDialog: AlertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+            val entryName: String = editTextEntryName.text.toString()
+            val entryValue: String = editTextEntryValue.text.toString()
+
+            if(entryName.isBlank()) {
+                Toast.makeText(this, "Enter an entry name first. Name cannot be blank.", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            if(entryAdapter.hasEntryWithName(entryName) &&  entryName != entry.first) {
+                Toast.makeText(this, "An entry with that name already exists!", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            entryAdapter.setAccountEntry(position, Pair(entryName, entryValue))
+            alertDialog.dismiss()
+        }
+
+        buttonPasswordGenerator.setOnClickListener {
+            showPasswordGeneratorDialog({alertDialog, generatedPassword ->
+                editTextEntryName.setText(generatedPassword)
+                alertDialog.dismiss()
+            }, { alertDialog, generatedPassword ->
+                editTextEntryValue.setText(generatedPassword)
+                alertDialog.dismiss()
+            })
+        }
+    }
+
+    private fun addEntry() {
+        val dialogEntryValuesInput: View = LayoutInflater.from(this).inflate(
+            R.layout.dialog_entry_values_input,
+            null
+        )
+
+        val editTextEntryName: TextView =
+            dialogEntryValuesInput.findViewById(R.id.dialogEntryValuesInputEditTextEntryName)
+
+        val editTextEntryValue: TextView =
+            dialogEntryValuesInput.findViewById(R.id.dialogEntryValuesInputEditTextEntryValue)
+
+        val buttonPasswordGenerator: Button =
+            dialogEntryValuesInput.findViewById(R.id.dialogEntryValuesInputButtonPasswordGenerator)
 
         val alertDialogBuilder: AlertDialog.Builder = AlertDialog.Builder(this).apply {
             setView(dialogEntryValuesInput)
@@ -131,52 +283,8 @@ class EntryViewer : AppCompatActivity() {
         alertDialog.show()
 
         alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
-            val entryName: String = dialogEntryValuesInputEditTextEntryName.text.toString()
-            val entryValue: String = dialogEntryValuesInputEditTextEntryValue.text.toString()
-
-            if(entryName.isBlank()) {
-                Toast.makeText(this, "Enter an entry name first. Name cannot be blank.", Toast.LENGTH_LONG).show()
-                return@setOnClickListener
-            }
-
-            if(entryAdapter.hasEntryWithName(entryName) &&  entryName != entry.first) {
-                Toast.makeText(this, "An entry with that name already exists!", Toast.LENGTH_LONG).show()
-                return@setOnClickListener
-            }
-
-            entryAdapter.setAccountEntry(position, Pair(entryName, entryValue))
-            alertDialog.dismiss()
-        }
-    }
-
-    private fun addEntry() {
-        val dialogNewEntry: View = LayoutInflater.from(this).inflate(
-            R.layout.dialog_entry_values_input,
-            null
-        )
-
-        val dialogNewEntryTextEditEntryName: TextView
-            = dialogNewEntry.findViewById(R.id.dialogEntryValuesInputEditTextEntryName)
-
-        val dialogNewEntryTextEditEntryValue: TextView
-            = dialogNewEntry.findViewById(R.id.dialogEntryValuesInputEditTextEntryValue)
-
-        val alertDialogBuilder: AlertDialog.Builder = AlertDialog.Builder(this).apply {
-            setView(dialogNewEntry)
-            setCancelable(true)
-
-            setTitle("Enter Entry Name")
-
-            setPositiveButton("Add")     { _, _  -> }
-            setNegativeButton("Cancel")  { _ , _ -> }
-        }
-
-        val alertDialog: AlertDialog = alertDialogBuilder.create()
-        alertDialog.show()
-
-        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
-            val entryName: String = dialogNewEntryTextEditEntryName.text.toString()
-            val entryValue: String = dialogNewEntryTextEditEntryValue.text.toString()
+            val entryName: String = editTextEntryName.text.toString()
+            val entryValue: String = editTextEntryValue.text.toString()
 
             if(entryName.isBlank()) {
                 Toast.makeText(this, "Enter an entry name first. Name cannot be blank.", Toast.LENGTH_LONG).show()
@@ -190,6 +298,16 @@ class EntryViewer : AppCompatActivity() {
 
             entryAdapter.addAccountEntry(entryName, entryValue)
             alertDialog.dismiss()
+        }
+
+        buttonPasswordGenerator.setOnClickListener {
+            showPasswordGeneratorDialog({alertDialog, generatedPassword ->
+                editTextEntryName.text = generatedPassword
+                alertDialog.dismiss()
+            }, { alertDialog, generatedPassword ->
+                editTextEntryValue.text = generatedPassword
+                alertDialog.dismiss()
+            })
         }
     }
 }
